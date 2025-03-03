205c4937615edd9f1a02937a5922682574e19f5c
micheledidonna01 <micheledidonna337@gmail.com>
2025-03-03 15:09:56 +0100

    feat: solution 02_iniziali

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,10 +4,19 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+firstLetterNames(names);
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+function firstLetterNames(namesPeople) {
+    let arrayLetters = [];
+    for (let i = 0; i < namesPeople.length; i++) {
+        let firstLetter = namesPeople[i].charAt(0);
+        arrayLetters.push(firstLetter);
+    }
+    return arrayLetters;
+}
+console.log(firstLetterNames(names));
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
