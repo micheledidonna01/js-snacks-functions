d30a1da498821a29c82babf1194ca2376848a4b6
micheledidonna01 <micheledidonna337@gmail.com>
2025-03-03 15:24:45 +0100

    feat: solution 03_filtra_iniziali

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,20 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+stringWithLetter(names);
 
 // Invoca la funzione qui e stampa il risultato in console
-
+function stringWithLetter(namesPeople){
+    let arrayNames = [];
+    for(let i = 0; i < namesPeople.length; i++){
+        const isWordWithAnLetter = namesPeople[i].startsWith('A');
+        if(isWordWithAnLetter){
+            arrayNames.push(namesPeople[i]);
+        }
+    }
+    return arrayNames;
+}
+console.log(stringWithLetter(names));
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
